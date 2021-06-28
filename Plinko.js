16df6053c369d57c8338e7cba9bb118646d3bfd9
RYAN5740A <81171121+RYAN5740A@users.noreply.github.com>
2021-06-28 18:29:51 +0530

    Update Plinko.js

--- Plinko.js
+++ Plinko.js
@@ -18,8 +18,7 @@ class Plinko {
         push();
         translate(pos.x, pos.y);
         rotate(angle);
-        imageMode(CENTER);
-        noStroke();
+        imageMode(CENTER)
         fill("white");
         ellipseMode(RADIUS);
         ellipse(0,0,this.r,this.r);
